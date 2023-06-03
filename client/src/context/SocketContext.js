465b707a500b3fadea392f4bf78f1993f9980938
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-06-03 09:48:40 +0100

    small changes

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -14,7 +14,7 @@ const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
 // // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8080';
+const URL = process.env.NODE_ENV 
 
     const socket = io(URL);
     setSocket(socket);
